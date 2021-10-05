ce8cc0d9776241a6a1bf902d3b054bf2bf014e76
Андронов <yamamotod@mail.ru>
2021-10-05 15:55:37 +0300

    HW2

---
+++ Task2_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 20:57:05 2021
+
+@author: yamamotod
+"""
+
+
+#Выяснить тип результата выражений:
+#15 * 3
+#15 / 3
+#15 // 2
+#15 ** 2
+
+print(type(15 * 3))
+print(type(15 / 3))
+print(type(15 // 3))
+print(type(15 ** 2))
+
+
+
+
+
+    
+    
+    
+    
+        
+       
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
